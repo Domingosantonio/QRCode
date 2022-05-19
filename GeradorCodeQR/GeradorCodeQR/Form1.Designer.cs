8d99fb878539f80098d132456bf7a1b2348616b0
Domingos António <71676771+Domingosantonio@users.noreply.github.com>
2022-05-19 18:43:05 +0100

    Add files via upload

---
+++ GeradorCodeQR/GeradorCodeQR/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace GeradorCodeQR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCodQR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picFoto = new System.Windows.Forms.PictureBox();
+            this.btnGerar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCodQR
+            // 
+            this.txtCodQR.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodQR.Location = new System.Drawing.Point(13, 41);
+            this.txtCodQR.Name = "txtCodQR";
+            this.txtCodQR.Size = new System.Drawing.Size(193, 22);
+            this.txtCodQR.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Conteúdo do Código QR";
+            // 
+            // picFoto
+            // 
+            this.picFoto.Location = new System.Drawing.Point(13, 69);
+            this.picFoto.Name = "picFoto";
+            this.picFoto.Size = new System.Drawing.Size(259, 210);
+            this.picFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picFoto.TabIndex = 2;
+            this.picFoto.TabStop = false;
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGerar.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGerar.Location = new System.Drawing.Point(213, 41);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(59, 23);
+            this.btnGerar.TabIndex = 3;
+            this.btnGerar.Text = "Gerar";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 301);
+            this.Controls.Add(this.btnGerar);
+            this.Controls.Add(this.picFoto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCodQR);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QR CODE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picFoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCodQR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picFoto;
+        private System.Windows.Forms.Button btnGerar;
+    }
+}
+
---
+++ GeradorCodeQR/GeradorCodeQR/Form1.cs
@@ -0,0 +1,45 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeradorCodeQR
+{
+    public partial class Form1 : Form
+    {
+        SaveFileDialog SaveArquivo = new SaveFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(txtCodQR.Text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            picFoto.Image = qrCodeImage;
+            //SALVADO 
+            SaveArquivo.Filter = "JPG (*.jpg)|*.jpg*";
+            SaveArquivo.AddExtension = true;
+            SaveArquivo.DefaultExt = ".jpg";
+            if (SaveArquivo.ShowDialog() == DialogResult.OK)
+            {
+                picFoto.Image.Save(SaveArquivo.FileName);
+                MessageBox.Show("O Code QR salvo com sucesso!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
